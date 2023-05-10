6970320f7000cfa9b19a5c093ec4d5da4f4fac50
AKHIL RAJ THAMMANA <AKHIL RAJ THAMMANA@DESKTOP-HPAH8SE>
2023-05-10 16:44:03 +0530

    second commit

---
+++ Practice/src/test/java/mavenpractice/Parameters.java
@@ -0,0 +1,32 @@
+package mavenpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Parameters {
+
+@Test(groups="smokeTest")
+public void m1()
+{
+String BROWSER = System.getProperty("browser");
+String URL = System.getProperty("url");
+System.out.println(BROWSER);
+System.out.println(URL);
+
+WebDriver driver;
+if(BROWSER.equalsIgnoreCase("chrome"))
+{
+	WebDriverManager.chromedriver().setup();
+	driver=new ChromeDriver();
+	
+}
+else
+{	
+	driver=new EdgeDriver();
+}
+driver.get(URL);
+}
+}
